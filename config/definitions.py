8e37e09413634290f981599cc32953a96b48b614
Radian-n <mattkirka320@gmail.com>
2023-01-09 00:05:08 +1300

    minor changes to model generation

--- config/definitions.py
+++ config/definitions.py
@@ -1,8 +1,8 @@
 import os
 
-# User set:
+# User setup:
 FILE_NAME = "Radian_n"  # Also expected twitch username. This is used to parse/clean file in remove_name() function
-CHANNEL_LIST = ["akiwoo", "radian_n", "akiwoobot"]
+CHANNEL_LIST = ["radian_n","akiwoo"]
 
 # Don't touch
 ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
